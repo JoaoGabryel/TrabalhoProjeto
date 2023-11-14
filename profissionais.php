192f6a6bef132180fa4c949505bc4a17e40d0b59
JoaoGabryel <sjoaogabryel@gmail.com>
2023-11-13 21:38:19 -0300

    aaaa

--- profissionais.php
+++ profissionais.php
@@ -1,6 +1,10 @@
 <?php
-    include "header.php";
-    include "config.php";
+include "header.php";
+include "config.php";
+$sql = "SELECT * FROM profissional";
+$consulta = $pdo->prepare($sql);
+$consulta->execute();
+$profissionais = $consulta->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
 <main>
@@ -8,29 +12,25 @@
     <div class="grid">
 
         <?php
-            foreach($produtos as $produto) { 
+        foreach ($profissionais as $profissional) {
         ?>
-           
-           <div class="coluna">
-                <img src="imagens/<?=$produto["imagem"]?>" alt="<?=$produto["nome"]?>">
-           
-                <p>
-                    <strong><?=$produto["nome"]?></strong><br>
-                    R$ <?=$produto["valor"]?>
-                </p>
 
-                <p>
-                    <a href="produto.php?id=<?=$produto["id"]?>">Detalhes</a>
-                </p>
+            <div class="card-body">
+                <img src="./imagens/<?= $profissional["foto"] ?>" alt="<?= $profissional["nome"] ?>" class="card-img-top">
+                <?= $profissional["clinica"] ?>
+                <?= $profissional["email"] ?>
             </div>
-
-        <?php 
-            }
-        ?>
-
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <p><a href="profissional.php?id=<?= $profissional["id"] ?>" class="btn btn-primary">Detalhes</a></p>
     </div>
+
+<?php
+        }
+?>
+
+</div>
 </main>
 
 <?php
-    include "footer.php";
+include "footer.php";
 ?>
