38113f9f77085a2b1c57239e62c1662e7e8bfcc9
Felipe <feligust150@gmail.com>
2023-11-13 21:05:55 -0300

    Top

--- index.php
+++ index.php
@@ -11,31 +11,19 @@ $profissionais = $consulta->fetchAll(PDO::FETCH_ASSOC);
     <h1>Profissionais Disponiveis:</h1>
     <div class="grid">
 
-        <?php
-        $i = 1;
-        shuffle($profissionais);
-        foreach ($profissionais as $profissional) {
-        ?>
-
-            <div class="coluna">
-                <img src="imagens/<?= $profissional["imagem"] ?>" alt="<?= $profissional["nome"] ?>">
-
-                <p>
-                    <strong><?= $profissional["nome"] ?></strong><br>
-                    <?= $profissional["email"] ?>
-                    <?= $profissional["clinica"] ?>
-                </p>
-
-                <p>
-                    <a href="produto.php?id=<?= $profissional["id"] ?>">Detalhes</a>
-                </p>
+        <?php $i = 1; shuffle($produtos); foreach($produtos as $produto) { ?>
+           
+           <div class="coluna card ">
+                <img src="imagens/<?=$produto["imagem"]?>" alt="<?=$produto["nome"]?>" class="card-img-top">
+                
+                <div class="card-body">
+                    <h5 class="card-title"><strong><?=$produto["nome"]?></strong></h5>
+                </div>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <p><a href="produto.php?id=<?=$produto["id"]?>" class="btn btn-primary">Detalhes</a></p>
             </div>
 
-        <?php
-            $i++;
-            if ($i > 4) break;
-        }
-        ?>
+        <?php $i++; if($i > 4) break;} ?>
 
     </div>
 </main>
