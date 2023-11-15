8417e7240fdb07b3bd164564816263c725680d9f
JoaoGabryel <sjoaogabryel@gmail.com>
2023-11-15 11:37:20 -0300

    aaa

--- sobre.php
+++ sobre.php
@@ -3,7 +3,7 @@ include "header.php";
 ?>
 
 <main>
-   
+
     <h1>Um Refúgio Virtual para o Autismo: Conectando Corações no Mundo Digital</h1>
 
     <p>No vasto universo da internet, surge um farol de esperança e acolhimento para aqueles que enfrentam o desafio do Transtorno do Espectro Autista (TEA). O site "Caminho Azul" se destaca como uma plataforma dedicada a oferecer suporte integral a pessoas com TEA, bem como às suas famílias e cuidadores.</p>
@@ -12,14 +12,13 @@ include "header.php";
     <p>O Caminho Azul vai além do virtual, organizando eventos online, webinars e sessões de terapia em grupo. Essas iniciativas promovem a interação em tempo real, criando uma atmosfera de solidariedade e aprendizado contínuo.</p>
     <p>Além disso, o site abriga uma seção exclusiva para recursos educacionais, incluindo atividades personalizadas e ferramentas interativas desenvolvidas para estimular o desenvolvimento cognitivo e social de indivíduos com TEA.</p>
     <p>O "Caminho Azul" não é apenas um site; é um refúgio virtual que transcende as barreiras da distância, proporcionando apoio tangível e emocional para aqueles que enfrentam o TEA. Ao unir conhecimento, compaixão e tecnologia, essa plataforma emerge como um farol de esperança e solidariedade na jornada única do autismo.</p>
+    <img src="./imagens/equipe.jpg" alt="" class="card-img-top">
 
-
-
-
-    <h1>Conheça nosso Mural de Pcientes</h1>
-
-    
-    <img src="./imagens/equipe.jpg" alt="">
+    <h1>Conheça nosso Mural de Pacientes</h1>
+    <img src="./imagens/crianca1.png" alt="" class="card-img-top">
+    <img src="./imagens/crianca2.jpg" alt="" class="card-img-top">
+    <img src="./imagens/crianca3.jpg" alt="" class="card-img-top">
+    <img src="./imagens/crianca4.jpg" alt="" class="card-img-top">
 </main>
 
 <?php
